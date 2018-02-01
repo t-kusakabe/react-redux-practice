4deeddea9a117b31df8e7546e529b3af6b0ae896
t-kusakabe <ttmac0704@gmail.com>
2018-02-01 18:26:41 +0900

    Create test component

--- src/app.jsx
+++ src/app.jsx
@@ -2,14 +2,9 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from  'react-redux'
 import { createStore } from 'redux'
-import todoApp from './reducers'
-import App from './components/App'
 
-let store = createStore(todoApp)
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <div>Test</div>,
   document.getElementById('root')
 )
