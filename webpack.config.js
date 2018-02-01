ac701b64132b2abe41c2cc7632eede6d21757170
t-kusakabe <ttmac0704@gmail.com>
2018-02-01 18:08:06 +0900

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: './src/app.jsx',
+  output: {
+    path: path.resolve('public/'),
+    filename: 'bundle.js'
+  },
+  devtool: 'inline-source-map',
+  resolve: {
+    modules: [path.join(__dirname, 'src'), 'node_modules'],
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js(x?)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, './public'),
+    inline: true,
+    port: 8080,
+    host: '0.0.0.0'
+  }
+}
