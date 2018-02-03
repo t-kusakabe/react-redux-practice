d974d4c6f272833187c75696a7d44724d79e6c49
t-kusakabe <ttmac0704@gmail.com>
2018-02-03 16:46:13 +0900

    Create component app

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Footer from './Footer'
+import AddTodo from '../containers/VisibleTodoList'
+
+const App = () => {
+  <div>
+    <AddTodo />
+    <VisibleTodoList />
+    <Footer />
+  </div>
+}
+
+export default App
